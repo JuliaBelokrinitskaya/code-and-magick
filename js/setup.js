3ea04624d2a5a4b12443c6b3a69775a894af7a1e
Julia Belokrinitskaya <bel_julia@mail.ru>
2018-04-14 05:31:45 +0300

    Add event handlers

--- js/setup.js
+++ js/setup.js
@@ -39,12 +39,55 @@ var EYES_COLORS = [
   'green'
 ];
 
+var FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
 /**
  * Количество похожих персонажей
  * @const
  * @type {number}
  */
 var SIMILAR_WIZARDS_COUNT = 4;
+
+/**
+ * Код клавиши Escape
+ * @const
+ * @type {number}
+ */
+var ESC_KEYCODE = 27;
+
+/**
+ * Код клавиши Enter
+ * @const
+ * @type {number}
+ */
+var ENTER_KEYCODE = 13;
+
+var setupWindow = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setupWindow.querySelector('.setup-close');
+var setupOpenIcon = setupOpen.querySelector('.setup-open-icon');
+var setupUserName = setupWindow.querySelector('.setup-user-name');
+var setupWizard = setupWindow.querySelector('.setup-wizard');
+var setupWizardEyes = setupWizard.querySelector('.wizard-eyes');
+var eyesColorField = setupWindow.querySelector('[name=eyes-color]');
+var setupFireball = setupWindow.querySelector('.setup-fireball-wrap');
+var fireballColorField = setupFireball.querySelector('[name=fireball-color]');
+var setupSimilar = setupWindow.querySelector('.setup-similar');
+var similarWizardsList = setupWindow.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var eyesColorValue = eyesColorField.value;
+var eyesColorIndex = EYES_COLORS.indexOf(eyesColorValue);
+var fireballColorValue = fireballColorField.value;
+var fireballColorIndex = FIREBALL_COLORS.indexOf(fireballColorValue);
+
+var similarWizards = [];
 
 /**
  * Функция, выбирающая случайный элемент в массиве.
@@ -120,17 +163,96 @@ var renderElements = function (dataList, parentElement, template, renderItem) {
 /**
  * Функция, показывающая окно настройки персонажа.
  */
-var showSetupWindow = function () {
-  var setupWindow = document.querySelector('.setup');
+var openSetupWindow = function () {
   setupWindow.classList.remove('hidden');
 
-  var similarWizardsList = setupWindow.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  setupOpen.removeEventListener('click', openSetupClickHandler);
+  setupOpenIcon.removeEventListener('keydown', openSetupEnterPressHandler);
 
-  var wizards = getWizardsList(SIMILAR_WIZARDS_COUNT);
-  renderElements(wizards, similarWizardsList, similarWizardTemplate, renderWizard);
-
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  document.addEventListener('keydown', setupWindowEscPressHandler);
 };
 
-showSetupWindow();
+/**
+ * Функция, скрывающая окно настройки персонажа.
+ */
+var closeSetupWindow = function () {
+  setupWindow.classList.add('hidden');
+
+  setupOpen.addEventListener('click', openSetupClickHandler);
+  setupOpenIcon.addEventListener('keydown', openSetupEnterPressHandler);
+
+  document.removeEventListener('keydown', setupWindowEscPressHandler);
+};
+
+/**
+ * Функция, определяющая следующий индекс элемента массива по кругу.
+ * @param {Array.<*>} items - массив элементов
+ * @param {number} currentIndex - индекс текущего элемента
+ * @return {number} - индекс следующего элемента (по кругу)
+ */
+var getNextIndex = function (items, currentIndex) {
+  return (currentIndex + 1) % items.length;
+};
+
+/**
+ * Функция, меняющая цвет глаз по порядку.
+ */
+var changeEyesColor = function () {
+  eyesColorIndex = getNextIndex(EYES_COLORS, eyesColorIndex);
+  eyesColorValue = EYES_COLORS[eyesColorIndex];
+
+  setupWizardEyes.style.fill = eyesColorValue;
+  eyesColorField.value = eyesColorValue;
+};
+
+/**
+ * Функция, меняющая цвет фаербола по порядку.
+ */
+var changeFireballColor = function () {
+  fireballColorIndex = getNextIndex(FIREBALL_COLORS, fireballColorIndex);
+  fireballColorValue = FIREBALL_COLORS[fireballColorIndex];
+
+  setupFireball.style.background = fireballColorValue;
+  fireballColorField.value = fireballColorValue;
+};
+
+var openSetupClickHandler = function () {
+  openSetupWindow();
+};
+
+var openSetupEnterPressHandler = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetupWindow();
+  }
+};
+
+var setupWindowEscPressHandler = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE && evt.target !== setupUserName) {
+    closeSetupWindow();
+  }
+};
+
+similarWizards = getWizardsList(SIMILAR_WIZARDS_COUNT);
+renderElements(similarWizards, similarWizardsList, similarWizardTemplate, renderWizard);
+setupSimilar.classList.remove('hidden');
+
+setupOpen.addEventListener('click', openSetupClickHandler);
+setupOpenIcon.addEventListener('keydown', openSetupEnterPressHandler);
+
+setupClose.addEventListener('click', function () {
+  closeSetupWindow();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeSetupWindow();
+  }
+});
+
+setupWizardEyes.addEventListener('click', function () {
+  changeEyesColor();
+});
+
+setupFireball.addEventListener('click', function () {
+  changeFireballColor();
+});
